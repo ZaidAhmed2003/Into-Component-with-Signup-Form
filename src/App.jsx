907d1bc88b26f7860528645c1c17dffc5a446657
Zaid Ahmed <zaidahmed2345@gmail.com>
2024-11-19 12:05:12 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg">
+        <div className="flex items-center justify-items-center">
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
